af0a42209f2d2dca36ba8f2740c1a2529db59084
mustafa-bilen <mustafabilen6669@gmail.com>
2022-09-28 00:59:34 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [hidden, setHidden] = useState(false);
+
+  const getQuote = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((res) => {
+        setQuote(res.data.content);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col h-screen w-screen justify-center items-center">
+      <div
+        onClick={() => getQuote()}
+        className="p-2 bg-slate-200 hover:shadow-lg rounded-lg cursor-pointer"
+      >
+        <button>Get Random Quote</button>
+      </div>
+      {quote && (
+        <div
+          className={`mt-4 px-10 py-4 bg-gray-200 rounded-lg shadow-xl ${
+            hidden ? `hidden` : "block"
+          }`}
         >
-          Learn React
-        </a>
-      </header>
+          {quote}
+        </div>
+      )}
+      <button
+        onClick={() => setQuote("")}
+        className="bg-red-200 p-2 rounded-lg mt-10"
+      >
+        Clear
+      </button>
+      <button
+        onClick={() => setHidden(!hidden)}
+        className="bg-red-200 p-2 rounded-lg mt-10"
+      >
+        {hidden ? "Show" : "Hidden"}
+      </button>
     </div>
   );
 }
